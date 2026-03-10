9d921b7c879e48d9642f2d625139ab79cf4b1d49
Kuinlin <quinlenmedelline@gmail.com>
2026-03-10 08:28:28 +0700

    wip: inplement student index page

--- app/core/Router.php
+++ app/core/Router.php
@@ -1,12 +1,12 @@
 <?php
 namespace App\Core;
- 
+
 use App\Controllers\StudentController;
- 
+
 class Router
 {
     private array $routes = [];
- 
+
     public function add(string $method, string $uri, string $controller, string $function)
     {
         $this->routes[] = [
@@ -20,9 +20,9 @@ public function run()
 {
     $method = $_SERVER['REQUEST_METHOD'];
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
- 
+
     foreach ($this->routes as $route) {
- 
+
         $pattern = str_replace(
             '{id}',
             '([0-9]+)',
@@ -30,25 +30,25 @@ public function run()
         );
         $pattern = '#^' . $pattern . '$#';
         // /students/{id} => /students/#^([0-9]+)$# = /students/1
- 
+
         if(preg_match($pattern, $uri, $matches))
         {
             array_shift($matches);
- 
+
             require_once '../app/controllers/' . $route['controller'] . '.php';
- 
+
             $controllerClass = 'App\\Controllers\\' . $route['controller'];
             $controller = new $controllerClass();
             $function = $route['function'];
- 
+
             call_user_func_array([$controller, $function], $matches);
             return;
         }
     }
- 
+
     http_response_code(404);
     echo '<h1>404 - Page Not Found</h1>';
 }
- 
+
 }
 ?>
