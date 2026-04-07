ea16feea55b2cccd5e06a4962a2c9e5f449fcf8c
Kuinlin <quinlenmedelline@gmail.com>
2026-04-07 07:57:42 +0700

    feat: implement student detail and edit page

--- app/views/students/create.php
+++ app/views/students/create.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,32 +7,32 @@
     <link rel="stylesheet" href="/css/output.css">
 </head>
 
-<body class="min-h-screen flex flex-col">
-    <!--header start-->
+<body class="min-h-screen flex flex-col bg-gray-100">
+    <!-- Header start -->
     <header class="bg-blue-500 text-white">
-        <div class="flex justify-between container mx-auto p-4">
+        <div class="flex items-center justify-between container mx-auto p-4">
             <a href="/students" class="font-bold text-xl">Sistem Sekolah</a>
             <a href="/students/create" class="bg-white text-blue-500 px-4 py-2 rounded-lg">+ Tambah Siswa</a>
         </div>
     </header>
-    <!--header end-->
+    <!-- Header End -->
 
-    <!--Main Start-->
-    <main class="bg-gray-100 grow container mx-auto">
-        <div class="mt-8 space-y-4">
-            <!-- Card Header Start-->
-            <div class="bg-white shadow rounded-lg-p-4">
-                <h1 class="text-2xl font-bold">Tambah Siswa</h1>
-                <p>Menabahkan siswa ke dalam sistem</p>
+    <!-- Main Content Start -->
+    <main class="grow container mx-auto p-4">
+        <div class="mt-8rem space-y-4">
+            <!-- Card Header Start -->
+            <div class="bg-white shadow rounded-lg p-4">
+                <h1 class="text-xl font-bold">Tambah Siswa</h1>
+                <p>Menambahkan siswa ke dalam sistem</p>
             </div>
-            <!-- Card Header End-->
+            <!-- Card Header End -->
 
-            <!--Card Content Start-->
+            <!-- Card Content Start -->
             <div class="bg-white shadow rounded-lg">
                 <form action="" class="p-4 grid grid-cols-2 gap-4">
                     <div class="space-y-2">
                         <label class="block font-bold" for="name">Nama</label>
-                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="name" id="name" placeholder="Masukkan Nama">
+                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="name" id="name" placeholder="Masukkan nama">
                     </div>
                     <div class="space-y-2">
                         <label class="block font-bold" for="nis">NIS</label>
@@ -41,11 +40,11 @@
                     </div>
                     <div class="space-y-2">
                         <label class="block font-bold" for="class">Kelas</label>
-                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="class" id="class" placeholder="Masukkan Kelas">
+                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="class" id="class" placeholder="Masukkan kelas">
                     </div>
                     <div class="space-y-2">
                         <label class="block font-bold" for="phone_number">No Telepon</label>
-                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="phone_number" id="phone_number" placeholder="Masukkan No Telepon">
+                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="phone_number" id="phone_number" placeholder="Masukkan no telepon">
                     </div>
                     <div class="flex justify-end gap-4 col-span-2">
                         <a href="/students" class="py-2 px-4 bg-gray-100 rounded-lg">Kembali</a>
@@ -53,18 +52,17 @@
                     </div>
                 </form>
             </div>
-            <!--Card Content End-->
+            <!-- Card Content End -->
         </div>
-        <!--Main End-->
     </main>
+    <!-- Main End -->
 
-    <!--footer start-->
-    <footer class="bg-gray-800 text-white">
+    <!-- Footer Start -->
+    <footer class="bg-gray-800 text-white ">
         <div class="text-center p-4">
-            &copy <?= date('Y') ?> - Sistem Sekolah SMK Kristem Immanuel
+            &copy <?= date('Y')?> - Sistem Sekolah SMK Kristen Immanuel
         </div>
     </footer>
-    <!--footer end-->
+    <!-- Footer End -->
 </body>
-
 </html>
